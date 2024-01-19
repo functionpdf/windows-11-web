b5e180a2bab6eab5770b27ce00d055e63e0a9827
cxz <102633295+functionpdf@users.noreply.github.com>
2024-01-19 12:06:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,55 +4,74 @@ function makeDraggableAndBringToFront(modalElement) {
     let offsetX, offsetY;
     let currentZIndex = 1;
 
-    // Bring the modal to the front when clicked
-    modalElement.addEventListener('click', function () {
+    function bringToFront() {
         modalElement.style.zIndex = currentZIndex++;
-    });
+    }
 
-    modalElement.addEventListener('mousedown', function (e) {
+    // Bring the modal to the front on single click
+    modalElement.addEventListener("mousedown", bringToFront);
+
+    modalElement.addEventListener("mousedown", function (e) {
         isDragging = true;
         offsetX = e.clientX - modalElement.offsetLeft;
         offsetY = e.clientY - modalElement.offsetTop;
 
         // Bring the modal to the front when it is clicked for dragging
-        modalElement.style.zIndex = currentZIndex++;
+        bringToFront();
     });
 
-    document.addEventListener('mousemove', function (e) {
+    document.addEventListener("mousemove", function (e) {
         if (isDragging) {
-            modalElement.style.left = e.clientX - offsetX + 'px';
-            modalElement.style.top = e.clientY - offsetY + 'px';
+            modalElement.style.left = e.clientX - offsetX + "px";
+            modalElement.style.top = e.clientY - offsetY + "px";
         }
     });
 
-    document.addEventListener('mouseup', function () {
+    document.addEventListener("mouseup", function () {
         isDragging = false;
     });
 
     // Close the modal
-    modalElement.querySelector('.closeWindow').addEventListener('click', function () {
-        modalElement.style.display = 'none';
+    modalElement.querySelector(".closeWindow").addEventListener("click", function () {
+        modalElement.style.display = "none";
     });
 }
 
+// Open Edge window
+document.addEventListener("DOMContentLoaded", function () {
+    let openEdgeButton = document.querySelector("#openEdgeWindow");
+
+    openEdgeButton.addEventListener("mousedown", function () {
+        let edgeModal = document.getElementById("edge");
+        edgeModal.style.display = "block";
+        makeDraggableAndBringToFront(edgeModal);
+    });
+});
+
 // Open Chrome window
 document.addEventListener("DOMContentLoaded", function () {
-    let openChromeButton = document.querySelector('#openChromeWindow');
+    let openChromeButton = document.querySelector("#openChromeWindow");
 
-    openChromeButton.addEventListener('dblclick', function () {
-        let chromeModal = document.getElementById('chrome');
-        chromeModal.style.display = 'block';
+    openChromeButton.addEventListener("mousedown", function () {
+        let chromeModal = document.getElementById("chrome");
+        chromeModal.style.display = "block";
         makeDraggableAndBringToFront(chromeModal);
     });
 });
 
 // Open File Explorer window
 document.addEventListener("DOMContentLoaded", function () {
-    let openFileExplorerButton = document.querySelector('#openFileExplorer');
+    let openFileExplorerButton = document.querySelector("#openFileExplorer");
 
-    openFileExplorerButton.addEventListener('dblclick', function () {
-        let fileExplorerModal = document.getElementById('fileExplorer');
-        fileExplorerModal.style.display = 'block';
+    openFileExplorerButton.addEventListener("mousedown", function () {
+        let fileExplorerModal = document.getElementById("fileExplorer");
+        fileExplorerModal.style.display = "block";
         makeDraggableAndBringToFront(fileExplorerModal);
     });
 });
+
+document.getElementById("openGithub").onclick = openGithub;
+
+function openGithub() {
+    window.open("https://github.com/functionpdf/windows-11-web");
+}
